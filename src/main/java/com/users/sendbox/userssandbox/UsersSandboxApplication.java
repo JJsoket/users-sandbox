3a74a112550af0f195241821852fe857940bd065
Иван <jouk0v2@mail.ru>
2018-12-13 19:54:06 +0300

    rep init

---
+++ src/main/java/com/users/sendbox/userssandbox/UsersSandboxApplication.java
@@ -0,0 +1,14 @@
+package com.users.sendbox.userssandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersSandboxApplication.class, args);
+	}
+
+}
+
